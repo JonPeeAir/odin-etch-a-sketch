82bf0bed03b779d4008305ebeac274fe7f6672eb
John Pierre Mendoza <johnpierremendoza@gmail.com>
2021-10-18 10:10:49 +0800

    canvas pixels now scale with the canvas
    number of canvas pixels can also be easily changed by modifying the numPixels variable

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const canvas = document.querySelector(".canvas");
+let canvasHeight = canvas.clientHeight;
+let canvasWidth = canvas.clientWidth;
+
+let numPixels = 16;
 let canvasPixels = [];
 
-for (let i = 0; i < 16; i++) {
+console.log(canvas.style.width);
+
+for (let i = 0; i < numPixels; i++) {
     canvasPixels.push([]);
 
-    for (let j = 0; j < 16; j++) {
-        canvasPixels[i].push(document.createElement("div"));
-        canvasPixels[i][j].setAttribute("class", "pixel");
-        canvasPixels[i][j].textContent = i + " " + j;
+    for (let j = 0; j < numPixels; j++) {
+        let canvasPixel = document.createElement("div");
+        canvasPixel.setAttribute("class", "pixel");
+        canvasPixel.style.height = `${canvasHeight / numPixels}px`;
+        canvasPixel.style.width = `${canvasWidth / numPixels}px`;
+        canvasPixel.addEventListener('mouseenter', () => {
+            canvasPixel.style.backgroundColor = "black";
+        });
+
+        canvasPixels[i].push(canvasPixel);
         canvas.appendChild(canvasPixels[i][j]);
     }
 }
